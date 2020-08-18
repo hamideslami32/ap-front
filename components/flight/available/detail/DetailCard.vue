1411704e1a007b5b84dd551d7e9c000e3aafb51c
Hosein Sharifinia <hosein.sharifinia@gmail.com>
2020-08-18 17:23:48 +0430

    add flight detail card

--- components/flight/available/detail/DetailCard.vue
+++ components/flight/available/detail/DetailCard.vue
@@ -2,6 +2,11 @@
     <div class="detail-card">
         <div class="detail-card__wrapper mb-4">
             <detail-item />
+            <div class="detail-card__stop p-2 my-3">
+                <svgicon name="chair" class="text-gray-700 p-1" width="18" height="18" />
+                <span class="text-weight-500 text-2 text-gray-900 mr-2">توقف در لندن به مدت 2 ساعت 40 دقیقه</span>
+            </div>
+            <detail-item />
         </div>
 
         <div class="detail-card__rule mb-5">
@@ -15,11 +20,16 @@
                 <div>قابل استرداد</div>
             </div>
         </div>
+
+        <div class="detail-card__tag">
+            <span class="text-primary text-1 text-weight-500 ">قوانین استرداد</span>
+        </div>
     </div>
 </template>
 
 <script>
 import DetailItem from '~/components/flight/available/detail/DetailItem'
+
 export default {
     name: 'DetailCard',
     components: {DetailItem}
@@ -31,12 +41,26 @@ export default {
         background: map-get($grays, '200');
         border-radius: 10px;
         border: 1px solid map_get($grays, '400');
+        position: relative;
 
         &__wrapper {
             border-radius: 10px;
             box-shadow: 0 5px 10px rgba(0, 0, 0, 0.05);
             background: $white;
             padding: 10px;
+        }
+
+        &__stop {
+            background: #e4f7f9;
+            border: 1px solid #c4edf1;
+            border-radius: 8px;
+
+            svg {
+                background: $white;
+                box-sizing: content-box;
+                border-radius: 50%;
+                box-shadow: 0 3px 5px rgba(0, 0, 0, 0.05);
+            }
         }
 
         &__rule {
@@ -60,7 +84,7 @@ export default {
                 box-sizing: content-box;
                 background: $white;
                 border-radius: 50%;
-                box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.05);
+                box-shadow: 0 3px 5px rgba(0, 0, 0, 0.05);
             }
 
             &__type {
@@ -71,6 +95,7 @@ export default {
                 background: linear-gradient(90deg, #45c6d4 0%, rgba(69, 198, 212, 0.1) 100%);
                 border-radius: 5px;
                 position: relative;
+
                 > div {
                     background: #e3f0f2;
                     border-radius: 5px;
@@ -83,6 +108,26 @@ export default {
                     top: 1px;
                 }
             }
+        }
+
+        &__tag {
+            position: absolute;
+            bottom: -1px;
+            height: 24px;
+            width: 100px;
+            background: transparent;
+            border: 1px solid $primary;
+            border-bottom-color: map_get($grays, '200');
+            border-top-color: map_get($grays, '200');
+            border-top-left-radius: 20px 30px;
+            border-top-right-radius: 20px 30px;
+            text-align: center;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            right: 0;
+            left: 0;
+            margin: auto;
         }
     }
 </style>
--- components/flight/available/detail/DetailItem.vue
+++ components/flight/available/detail/DetailItem.vue
@@ -13,12 +13,54 @@
             <svgicon name="plane-takeoff" class="text-gray-700 flex-grow-1" width="40" height="20" />
             <span class="text-2 text-gray-800 font-en text-weight-500">London</span>
         </div>
+        <div class="detail-item__time d-flex align-items-center justify-content-between mt-3 mb-2">
+            <div class="detail-item__time__holder">
+                <p class="text-weight-600 text-gray-900 mb-0 py-1">
+                    23:00
+                </p>
+                <p class="font-en text-1 text-gray-800 text-weight-500 mb-0 py-1">
+                    23 AUG
+                </p>
+            </div>
+            <div class="flex-grow-1 mt-2 px-3">
+                <div class="detail-item__time__route px-3">
+                    <span class="text-2 text-weight-500 font-en text-gray-700">2h 30m</span>
+                </div>
+                <div class="text-1 text-gray-800 text-weight-500 mt-4 d-flex align-items-center justify-content-between">
+                    <span>۲۰ مرداد ۹۹</span>
+                    <span>۰۴ شهریور ۹۹</span>
+                </div>
+            </div>
+            <div class="detail-item__time__holder">
+                <p class="text-weight-600 text-gray-900 mb-0 py-1">
+                    23:00
+                </p>
+                <p class="font-en text-1 text-gray-800 text-weight-500 mb-0 py-1">
+                    23 AUG
+                </p>
+            </div>
+        </div>
+        <div class="d-flex align-items-center justify-content-between">
+            <badge class="px-3">
+                IKA
+            </badge>
+            <div class="d-flex align-items-center flex-grow-1 px-3 justify-content-between font-en text-weight-500 text-2 text-gray-700">
+                <span>Imam Khomeini</span>
+                <span>Heathrow</span>
+            </div>
+            <badge class="px-3">
+                LHR
+            </badge>
+        </div>
     </div>
 </template>
 
 <script>
+import Badge from '~/components/ui/Badge'
+
 export default {
-    name: 'DetailItem'
+    name: 'DetailItem',
+    components: {Badge}
 }
 </script>
 
@@ -33,6 +75,18 @@ export default {
         bottom: 0;
     }
 
+    @mixin circle {
+        content: '';
+        width: 12px;
+        height: 12px;
+        background: $white;
+        border-radius: 50%;
+        position: absolute;
+        bottom: 0;
+        border: 1px solid map_get($grays, '400');
+        top: -6px;
+    }
+
     .detail-item {
         &__airplane {
             display: flex;
@@ -44,8 +98,9 @@ export default {
             padding: 5px 10px;
             border-radius: 8px;
             border: 1px solid map_get($grays, '500');
+
             span:last-child { // image section for airline logo
-                background: rgba(255,0, 0, 0.1);
+                background: rgba(255, 0, 0, 0.1);
                 width: 30px;
                 height: 30px;
                 border-radius: 50px;
@@ -59,14 +114,64 @@ export default {
 
             span {
                 position: relative;
+
                 &:first-child {
                     margin-right: 10px;
+
                     &:before {
                         @include line;
                         right: -10px;
                     }
                 }
             }
+        }
+
+        &__time {
+            &__holder {
+                background: map_get($grays, '500');
+                border: 1px solid map_get($grays, '500');
+                border-radius: 5px;
+                min-width: 55px;
+                text-align: center;
+
+                p {
+                    &:first-child {
+                        background: $white;
+                        border-radius: 5px;
+                        margin: 1px;
+                    }
+                }
+            }
+
+            &__route {
+                border-top: 1px dashed map_get($grays, '400');
+                position: relative;
+
+                > span {
+                    background: $white;
+                    border: 1px solid map_get($grays, '500');
+                    padding: 1px 10px;
+                    border-radius: 100px;
+                    position: absolute;
+                    top: -10px;
+                    left: 50%;
+                    transform: translateX(-50%);
+                }
+
+                &:before {
+                    @include circle;
+                    right: 0;
+                }
+
+                &:after {
+                    @include circle;
+                    left: 0;
+                }
+            }
+        }
+
+        /deep/ .badge {
+            font-size: 0.75rem;
         }
     }
 </style>
