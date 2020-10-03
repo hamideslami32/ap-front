a4cef69bc5d099a5526257412d457190f1df2e8e
Hosein Sharifinia <hosein.sharifinia@gmail.com>
2020-10-03 12:02:10 +0330

    feat: add info for contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,24 +1,26 @@
 <template>
     <div class="container">
-        <section class="mt-3">
+        <section class="mt-4">
             <h3 class="mb-0 text-gray-1000 text-weight-500">
                 تماس با ما
             </h3>
-            <small class="text-primary">(سفری ارزان با اپرو)</small>
+            <!--            <small class="text-primary">(سفری ارزان با اپرو)</small>-->
             <p class="mt-4 text-grays-1000">
-                <span class="text-weight-600 text-gray-900 ml-2">شماره تماس:</span> ۲۰۰۰۰۰۰۰۰
+                <span class="text-weight-600 text-gray-900 ml-2">شماره تماس:</span> <a dir="ltr" href="tel:021-72475">۰۲۱-۷۲۴۷۵</a>
+                - <a href="tel:09219601487">۰۹۲۱۹۶۰۱۴۸۷</a>
             </p>
-            <p class="text-grays-1000">
+            <!--            <p class="text-grays-1000">
                 <span class="text-weight-600 text-gray-900 ml-2">شماره فکس:</span> ۲۰۰۰۰۰۰۰۰
+            </p>-->
+            <p class="text-grays-1000">
+                <span class="text-weight-600 text-gray-900 ml-2">پست الکترونیکی:</span> Info@galactictravel.ir
             </p>
             <p class="text-grays-1000">
-                <span class="text-weight-600 text-gray-900 ml-2">پست الکترونیکی:</span> ۲۰۰۰۰۰۰۰۰
-            </p>
-            <p class="text-grays-1000">
-                <span class="text-weight-600 text-gray-900 ml-2">آدرس:</span>تهران - اتوبان همت - اولین بریدگی - درخت کاج
+                <span class="text-weight-600 text-gray-900 ml-2">آدرس:</span> تهران، یوسف آباد، خیابان اسدآبادی، نبش
+                خیابان چهاردهم، ساختمان ویرا، طبقه چهارم
             </p>
         </section>
-        <section class="mt-5">
+        <!--        <section class="mt-5 mb-3">
             <h5 class="text-center text-weight-500 text-primary">
                 فرم تماس با ما
             </h5>
@@ -37,16 +39,16 @@
                     ارسال
                 </a-btn>
             </form>
-        </section>
+        </section>-->
     </div>
 </template>
 
 <script>
-import AInput from '~/components/ui/form/AInput'
+// import AInput from '~/components/ui/form/AInput'
 
 export default {
-    name: 'Contact',
-    components: {AInput}
+    name: 'Contact'
+    // components: {AInput}
 }
 </script>
 
