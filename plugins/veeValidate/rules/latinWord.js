7fd6bd5bad790687968412a2451de78b74933f0f
Hamid Eslami <heslami32@gmail.com>
2020-10-10 16:44:44 +0330

    fix: latin regex for passengers form

--- plugins/veeValidate/rules/latinWord.js
+++ plugins/veeValidate/rules/latinWord.js
@@ -2,7 +2,7 @@ import {extend} from 'vee-validate'
 
 extend('latinWord', {
     validate(value) {
-        return /^[a-zA-Z]+$/.test(value)
+        return /^[a-zA-Z\s]+$/.test(value)
     },
     message: '{_field_} را به صورت لاتین وارد نمایید',
     computesRequired: true
