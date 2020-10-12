656ba57a2cabe165e9f4cf539c56064cf84447f1
Sasan Farrokh <s.farrokh@alibaba.ir>
2020-10-12 23:22:29 +0330

    fix: api url

--- plugins/api.js
+++ plugins/api.js
@@ -2,6 +2,7 @@ export class BaseApi {}
 
 export default function(ctx) {
     const axios = ctx.app.$axios
+    ctx.app.$axios.defaults.baseURL = ctx.$config.API_URL
     axios.defaults.headers.common.Timezone = -new Date().getTimezoneOffset()
     BaseApi.prototype.axios = axios
 }
